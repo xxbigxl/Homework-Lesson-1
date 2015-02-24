3b2bb46ca784e0a25868f833e159777c7213ecd6
xxbigxl <xxbigxl@gmail.com>
2015-02-25 00:07:21 +0300

    +1 50%

---
+++ src/com/example/tests/FillTheFieldParameter.java
@@ -0,0 +1,33 @@
+package com.example.tests;
+
+public class FillTheFieldParameter {
+	public String name;
+	public String lastname;
+	public String address;
+	public String home;
+	public String mobile;
+	public String work;
+	public String email;
+	public String byear;
+	public String address2;
+	public String phone2;
+
+	public FillTheFieldParameter(String name, String lastname,
+			String address, String home, String mobile, String work,
+			String email, String byear, String address2, String phone2) {
+		this.name = name;
+		this.lastname = lastname;
+		this.address = address;
+		this.home = home;
+		this.mobile = mobile;
+		this.work = work;
+		this.email = email;
+		this.byear = byear;
+		this.address2 = address2;
+		this.phone2 = phone2;
+	}
+	
+	public FillTheFieldParameter() {
+	}
+	
+}
